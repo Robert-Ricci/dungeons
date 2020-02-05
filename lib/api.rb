830b6086efb85b3d585866a6a4ddff27817ba976
rmricci121 <rmricci121@gmail.com>
2020-02-04 21:18:57 -0500

    added contoller welcome message

--- lib/api.rb
+++ lib/api.rb
@@ -4,8 +4,8 @@ class Dungeons::Api
 
 
     def api_call
-        #binding.pry 
-        url = 'http://www.dnd5eapi.co/api/races/{index}'
+        binding.pry
+        url = 'http://www.dnd5eapi.co/api/races'
         response = HTTParty.get(url)
         response.parsed_response
     end
--- lib/dungeon_controller.rb
+++ lib/dungeon_controller.rb
@@ -3,6 +3,9 @@ class Dungeons::DungeonController
 
     def welcome
         puts "Welcome Adventurers to my D&D CLI!"
+        puts "For Races type 'races'"
+        puts "For Classes type 'classes'"
+        puts "To exit type 'exit'"
     end
 
 end
